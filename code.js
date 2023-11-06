c06661fd5d0a9c50d3ab280fb503f34c0d306545
CadeMaynard <143521268+CadeMaynard@users.noreply.github.com>
2023-11-06 13:24:02 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
-function hasCycle(graph) {
-    return false;
+function hasCycle(graph, v1, v2) {
+    if(v1 == v2){
+        return true;
+    } else if(graph[v1] == [] || graph[v1][graph[v1].length - 1] == -1) {
+        return false;
+    } else {
+        graph[v1][graph[v1].length] = -1 // This is how I have opted to mark vertices as visited.
+        found = false;
+        for(i = 0; i < graph[v1].length - 1 && found == false; i++){
+            found = hasCycle(graph, graph[v1][i], v2);
+        }
+        return found;
+    }
 }
