e794cb7d4cfd95cfd0b26424ee1e433b4ab60855
CadeMaynard <143521268+CadeMaynard@users.noreply.github.com>
2023-11-06 13:27:55 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
-function hasCycle(graph, v1, v2) {
+function hasCycle(graph, v1, v2) {  //v1 and v2 are vertices, the idea is that the function answers the question 
+                                    //will there be a cycle if I place an edge between these two vertices
     if(v1 == v2){
         return true;
     } else if(graph[v1] == [] || graph[v1][graph[v1].length - 1] == -1) {
